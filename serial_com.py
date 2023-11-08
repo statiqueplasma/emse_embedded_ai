a6e10156a71b76b33c2a0e59c27c65c3b92bd329
statiqueplasma <h.aouadidrissi@gmail.com>
2023-11-08 16:18:48 -0500

    last edits

--- serial_com.py
+++ serial_com.py
@@ -1,47 +1,102 @@
 import serial
 import numpy as np
 import time
-ser = serial.Serial(
-    # Prot used by your computer
-   port='/dev/ttyACM0',
-   baudrate=115200,
-   parity=serial.PARITY_ODD,
-   stopbits=serial.STOPBITS_TWO,
-   bytesize=serial.SEVENBITS
-)
+import struct
+
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+
+def arr_to_string(array):
+    buff = ""
+    for elem in array:
+        if buff != "":
+            buff += ':'
+        buff += str(elem)
+    return buff
+
 
 Y_data = np.load('WINE_QUALITY_Y_TEST.npy')
 X_data = np.load('WINE_QUALITY_X_TEST.npy')
 
 success_count = 0
 fail_count = 0
-try:
-    while 1:
-        # get keyboard input
-        # input = input(">> ")
-        # if input == 'exit':
-        #     ser.close()
-        #     exit()
-        
-        # for i in range(len(Y_data)):
-        #     data_to_send = X_data
-        #     ser.write(data_to_send + '\r\n')
-        #     out = ''
-        #     time.sleep(1)
-        #     while ser.inWaiting() > 0:
-        #         out += ser.read(1)
-        #     if out == Y_data[i]:
-        #         success_count = success_count + 1
-        #     else:
-        #         fail_count = fail_count + 1
 
-        out = ''
-        while ser.inWaiting() > 0:
-            out += ser.read(1).decode()
-        if out != '':
-            print('>> ', out)
-        if out == "test 101":
-            ser.write("ok".encode())
 
-except KeyboardInterrupt:
-    ser.close()
+def main():
+    ser = serial.Serial(
+        # Prot used by your computer
+        port='/dev/ttyACM0',
+        baudrate=115200,
+        parity=serial.PARITY_ODD,
+        stopbits=serial.STOPBITS_TWO,
+        bytesize=serial.SEVENBITS
+    ) 
+    state = "start"
+    try:
+        while 1:
+            out = ''
+            if state == "start":
+                print("Program Starting ... \n")
+                ser.flush()
+                state = "send"
+            
+            if state == "send":
+                
+                printProgressBar(0, 12, prefix = 'Sending Data:', suffix = 'Complete', length = 50)
+                for i in range(len(X_data[0])):
+                    out = ''
+                    ser.write((bytes(X_data[0][i])))
+                    printProgressBar(i+1, 12, prefix = 'Sending Data:', suffix = 'Complete', length = 50)
+                state = "receive"
+            
+            if state == "receive":
+                print(f'\rWaiting for prediction... \n')
+                output = np.zeros((1,7))
+                sync = b"000"
+                while(sync != b"010"):
+                    sync = ser.read(3)
+                for i in range(7):
+                    prediction = struct.unpack('f', ser.read(4))[0]
+                    output[0][i] = prediction
+                if output[0][6] != 0:
+                    print("Expectation : ", Y_data[0])
+                    print("Predictions : \n\n")
+                    print("============================ \n")
+                    output_display = output.round(decimals=4)
+                    for i in range(7):
+                        print("-> Label : {} | prediction : {}\n".format(i,output_display[0][i]))
+                    print("============================ \n")
+                    state = "stop"
+            
+
+    except KeyboardInterrupt:
+        ser.close()
+    
+
+
+
+
+if __name__ == '__main__':
+    main()
+
